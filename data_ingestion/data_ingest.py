713f7b4dd908968a448fd0ae4e0deab69662d994
shilpa07-ingole <shilpa.ingole@gmail.com>
2025-04-26 21:50:08 +0530

    data updated transformation

--- data_ingestion/data_ingest.py
+++ data_ingestion/data_ingest.py
@@ -1,6 +1,15 @@
-from langchain-astradb import AstraDBVectorStore
+from langchain_astradb import AstraDBVectorStore
 from dotenv import load_dotenv
 import os
 import pandas as pd
 from data_ingestion.data_transform import data_converter
 
+class ingest_data:
+    def __init__(self):
+        print("data ingestion class has been intialize")
+
+    def data_ingestion(self):
+        pass
+
+if __name__=='__main__':
+    data_ingestion=ingest_data()
--- data_ingestion/data_transform.py
+++ data_ingestion/data_transform.py
@@ -4,11 +4,14 @@ from langchain_core.documents import Document
 # code for converting data
 class data_converter:
     def __init__(self):
-        pass
+        print("data converter path has been intialize")
 
 
     def data_transformation(self):
         pass
+
+if __name__=='__main__':
+    data_convert=data_converter()
 
 
         
