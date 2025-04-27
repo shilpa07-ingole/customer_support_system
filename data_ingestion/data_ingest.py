900fdf3275097f9701054dd28dc6b51f687b2eca
shilpa07-ingole <shilpa.ingole@gmail.com>
2025-04-27 18:18:12 +0530

    added code langchain-core

--- data_ingestion/data_ingest.py
+++ data_ingestion/data_ingest.py
@@ -4,12 +4,42 @@ import os
 import pandas as pd
 from data_ingestion.data_transform import data_converter
 
+load_dotenv()
+
+GOOGLE_API_KEY=os.getenv("GOOGLE_API_KEY")
+ASTRA_DB_API_ENDPOINT=os.getenv("ASTRA_DB_API_ENDPOINT")
+ASTRA_DB_APPLICATION_TOKEN=os.getenv("ASTRA_DB_APPLICATION_TOKEN")
+ASTRA_DB_KEYSPACE=os.getenv("ASTRA_DB_KEYSPACE")
+
+os.environ["GOOGLE_API_KEY"]=GOOGLE_API_KEY
+os.environ["ASTRA_DB_API_ENDPOINT"]=ASTRA_DB_API_ENDPOINT
+os.environ["ASTRA_DB_APPLICATION_TOKEN"]=ASTRA_DB_APPLICATION_TOKEN
+os.environ["ASTRA_DB_KEYSPACE"]=ASTRA_DB_KEYSPACE
+
 class ingest_data:
     def __init__(self):
         print("data ingestion class has been intialize")
+        self.embeddings = GoogleGenerativeAIEmbeddings(model="models/text-embedding-004")
+        self.data_conveter=data_conveter()
 
     def data_ingestion(self):
-        pass
+        vstore=AstraDBVectorStore(
+            embedding=self.embeddings,
+            collection_name="customer_support",
+            api_endpoint=ASTRA_DB_API_ENDPOINT,
+            token=ASTRA_DB_APPLICATION_TOKEN,
+            namespace=ASTRA_DB_KEYSPACE,
+        )
+        storage=status
+        
+        if storage==None:
+            docs=self.data_conveter.data_transformation()
+            inserted_ids=vstore.add_documents(docs)
+            print(inserted_ids)
+        else:
+            return vstore
+        
+        return vstore,inserted_ids
 
 if __name__=='__main__':
     data_ingestion=ingest_data()
--- data_ingestion/data_transform.py
+++ data_ingestion/data_transform.py
@@ -6,13 +6,13 @@ class data_converter:
     def __init__(self):
         print("data converter path has been intialize")
         self.product_data=pd.read_csv(r"C:\Users\SHILPA\customer_support_system\data\amazon_product_review.csv")
-        print(self.product_data.head())
+        #print(self.product_data.head())
 
 
     def data_transformation(self):
-        reuired_columns=self.product_data.columns
-        required_columns=list(reuired_columns[1:])
-
+        required_cols=self.product_data.columns
+        required_columns=list(required_cols[1:])
+        #print(required_columns)
         product_list=[]
         for index,row in self.product_data.iterrows():
             object={
@@ -20,22 +20,23 @@ class data_converter:
                 "product_rating":row["rating"],
                 "product_summary":row["summary"],
                 "product_review":row["review"]
-
-
             }
             product_list.append(object)
-        print("******below is product list****")
-        print(product_list)
-
+        #print(product_list)
+        docs=[]
         for entry in product_list:
-            metadata={"product_name":entry["product_name","product_rating":entry["product_rating"]]}
-            doc=Document(page_content=entry["product_name"],metadata=metadata)
+            metadata={
+                "product_name":entry["product_name"],"product_rating":entry["product_rating"],"product_review":entry["product_review"],"product_summary":entry["product_summary"]
+            }
+            doc=Document(page_content=entry["product_review"],metadata=metadata)
             docs.append(doc)
-        print(docs[0])
+            print(docs[0])
 
+        
 
 if __name__=='__main__':
     data_convert=data_converter()
+    data_convert.data_transformation()
 
 
         
